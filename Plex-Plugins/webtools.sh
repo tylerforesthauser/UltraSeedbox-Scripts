51a810bf82b496313d8987d24ab20e5e6e1a266d
Xan <xanban@smallmilky.club>
2020-01-11 01:27:20 +0800

    Add Plex-Plugins

---
+++ Plex-Plugins/webtools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+plugindir=~/.config/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/
+
+if [ -f "$HOME/.config/plex/" ];
+then
+    echo "Plex is not installed. Exiting..."
+    exit
+fi
+
+echo "Installing WebTools..."
+cd "$plugindir" || exit
+git clone https://github.com/ukdtom/WebTools.bundle.git
+app-plex restart
+sleep 15
+echo "Webtools installed!"
+exit
