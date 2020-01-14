29841a92273f4dd9de3b699a77fac83895d454e4
Xan <xanban@smallmilky.club>
2020-01-14 18:29:33 +0800

    added mergerfs check

--- MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
+++ MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-mkdir -p ~/tmp
-wget https://github.com/trapexit/mergerfs/releases/download/2.28.3/mergerfs_2.28.3.debian-stretch_amd64.deb -P ~/tmp
-dpkg -x ~/tmp/mergerfs_2.28.3.debian-stretch_amd64.deb ~/tmp
-mv ~/tmp/usr/bin/* ~/bin
-rm -rf ~/tmp
-which mergerfs
-mergerfs -v
+
+if pgrep "mergerfs";
+then
+    echo "mergerfs is running. Please close all mergerfs instances before proceeding."
+    exit
+else
+    echo "mergerfs is installing/upgrading..."
+    mkdir -p "$HOME"/tmp
+    wget https://github.com/trapexit/mergerfs/releases/download/2.28.3/mergerfs_2.28.3.debian-stretch_amd64.deb -P "$HOME"/tmp
+    dpkg -x "$HOME"/tmp/mergerfs_2.28.3.debian-stretch_amd64.deb "$HOME"/tmp
+    mv "$HOME"/tmp/usr/bin/* "$HOME"/bin
+    rm -rf "$HOME"/tmp
+    echo "Done."
+    command -v mergerfs
+    mergerfs -v
+    exit
+fi
