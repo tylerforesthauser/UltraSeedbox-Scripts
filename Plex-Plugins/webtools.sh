7d5d7837da60ec44f9c20c3fb188a238f94d7861
Xan <xanban@smallmilky.club>
2020-01-14 22:37:39 +0800

    Added upgrade lines

--- Plex-Plugins/webtools.sh
+++ Plex-Plugins/webtools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-plugindir=~/.config/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/
+plugindir=$HOME/.config/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/
+supportdir=$HOME/.config/plex/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/
 
 if [ ! -d "$HOME/.config/plex/" ];
 then
@@ -8,10 +9,24 @@ then
     exit
 fi
 
-echo "Installing WebTools..."
-cd "$plugindir" || exit
-git clone https://github.com/ukdtom/WebTools.bundle.git
-app-plex restart
-sleep 15
-echo "Webtools installed!"
-exit
+if [ -d "$plugindir/WebTools.bundle" ];
+then
+  echo "Webtools Found. Upgrading..."
+  cd "$plugindir/WebTools.bundle" || exit
+  git pull
+  rm -rf "$supportdir/Caches/com.plexapp.plugins.WebTools"
+  rm -rf "$supportdir/Data/com.plexapp.plugins.WebTools"
+  rm -rf "$supportdir/Preferences/com.plexapp.plugins.WebTools.xml"
+  app-plex restart
+  sleep 15
+  echo "Webtools Upgraded."
+  exit
+else
+    echo "Installing WebTools..."
+    cd "$plugindir" || exit
+    git clone https://github.com/ukdtom/WebTools.bundle.git
+    app-plex restart
+    sleep 15
+    echo "Webtools installed."
+    exit
+fi
