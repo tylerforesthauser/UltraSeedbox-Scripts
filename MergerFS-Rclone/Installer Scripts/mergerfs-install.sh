ad4b07bebc8443cbda71f30ff4f3296e0e6f1734
Xan <git@xanban.xyz>
2020-03-19 20:34:25 +0800

    Update to 2.29.0

--- MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
+++ MergerFS-Rclone/Installer Scripts/mergerfs-install.sh
@@ -7,12 +7,13 @@ then
 else
     echo "mergerfs is installing/upgrading..."
     mkdir -p "$HOME"/tmp
-    wget https://github.com/trapexit/mergerfs/releases/download/2.28.3/mergerfs_2.28.3.debian-stretch_amd64.deb -P "$HOME"/tmp
-    dpkg -x "$HOME"/tmp/mergerfs_2.28.3.debian-stretch_amd64.deb "$HOME"/tmp
+    wget https://github.com/trapexit/mergerfs/releases/download/2.29.0/mergerfs_2.29.0.debian-stretch_amd64.deb -P "$HOME"/tmp
+    dpkg -x "$HOME"/tmp/mergerfs_2.29.0.debian-stretch_amd64.deb "$HOME"/tmp
     mv "$HOME"/tmp/usr/bin/* "$HOME"/bin
     rm -rf "$HOME"/tmp
     echo "Done."
     command -v mergerfs
     mergerfs -v
+    echo "Install done. Restart all mergerfs mounts."
     exit
 fi
