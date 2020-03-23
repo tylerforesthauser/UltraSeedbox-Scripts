145fe3f9a6c595915360c28a5e3e0676a1e4d90b
Xan <git@xanban.xyz>
2020-03-24 01:05:36 +0800

    added service-file-check.sh

--- MergerFS-Rclone/Service Files/service-file-check.sh
+++ MergerFS-Rclone/Service Files/service-file-check.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
-echo "What do you want to do?"
-echo "1) Show ExecStart"
-echo "2) Show ExecStop"
-echo "3) Execute ExecStart"
-echo "4) Exit"
-read -r -p "Select an option: " option
+  echo "What do you want to do?"
+  echo "1) Show ExecStart"
+  echo "2) Show ExecStop"
+  echo "3) Execute ExecStart"
+  echo "4) Exit"
 
+while true :
+do
+read -rp "Select an option: " option
 case $option in
   1)
-    input="y"
-    while [ "$input" = "y" ]
-    do
     cat "$HOME"/.config/systemd/user/rclone-vfs.service | sed -n '/^ExecStart=/,$p' | sed -n '/^ExecStop=/q;p' | sed 's/^ExecStart=//g'
-    done
     ;;
   2)
-    input="y"
-    while [ "$input" = "y" ]
-    do
     cat "$HOME"/.config/systemd/user/rclone-vfs.service | sed -n '/^ExecStop=/,$p' | sed -n '/^Restart=/q;p' | sed 's/^ExecStop=//g'
-    done
     ;;
   3)
-    input="y"
-    while [ "$input" = "y" ]
-    do
-    cat "$HOME"/.config/systemd/user/rclone_vfs.service | sed -n '/^ExecStart=/,$p' | sed -n '/^ExecStop=/q;p' | sed 's/^ExecStart=//g' | bash
-    done
+    cat "$HOME"/.config/systemd/user/rclone-vfs.service | sed -n '/^ExecStart=/,$p' | sed -n '/^ExecStop=/q;p' | sed 's/^ExecStart=//g' | bash
     ;;
   4)
-    exit
+    echo "Bye."
+    exit 0
     ;;
-esac
+  *) echo "Wrong option $option"
+  esac
+done
